2539a4bd97b3939879dab041f2390aa894efa748
SWASTIK-WD <swastikranjanmandal@gmail.com>
2025-08-16 16:42:24 +0530

    Resume feedback and review components

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -6,7 +6,7 @@ import ATS from "~/components/ATS";
 import Details from "~/components/Details";
 
 export const meta = () => ([
-    { title: 'Resumind | Review ' },
+    { title: 'ProFileIQ | Review ' },
     { name: 'description', content: 'Detailed overview of your resume' },
 ])
 
@@ -77,7 +77,7 @@ const Resume = () => {
                         <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
                             <Summary feedback={feedback} />
                             <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
-                            <Details feedback = {feedback} />
+                            <Details feedback ={feedback} />
                         </div>
                     ) : (
                         <img src="/images/resume-scan-2.gif" className="w-full" />
